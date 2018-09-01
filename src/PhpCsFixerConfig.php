30444519b1583dd066dd868ed5ce9891b60c8c75
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-09-02 00:47:35 +0200

    Use config from api-clients/cs-fixer-config

--- src/PhpCsFixerConfig.php
+++ src/PhpCsFixerConfig.php
@@ -2,48 +2,17 @@
 
 namespace ApiClients\Tools\TestUtilities;
 
+use ApiClients\Tools\CsFixerConfig\PhpCsFixerConfig as CSFC;
 use PhpCsFixer\Config;
 
 final class PhpCsFixerConfig
 {
     /**
      * @return Config
+     * @deprecated use ApiClients\Tools\CsFixerConfig\PhpCsFixerConfig directly
      */
     public static function create(array $extraRules = []): Config
     {
-        return Config::create()
-            ->setRules(
-            [
-                '@PSR2' => true,
-                'array_syntax' => [
-                    'syntax' => 'short',
-                ],
-                'indentation_type' => true,
-                'blank_line_before_return' => true,
-                'declare_strict_types' => true,
-                'method_separation' => true,
-                'new_with_braces' => true,
-                'no_extra_consecutive_blank_lines' => true,
-                'no_leading_import_slash' => true,
-                'no_short_bool_cast' => true,
-                'no_unused_imports' => true,
-                'no_whitespace_before_comma_in_array' => true,
-                'no_whitespace_in_blank_line' => true,
-                'ordered_class_elements' => true,
-                'ordered_imports' => true,
-                'phpdoc_add_missing_param_annotation' => true,
-                'phpdoc_align' => true,
-                'phpdoc_no_empty_return' => true,
-                'phpdoc_order' => true,
-                'phpdoc_scalar' => true,
-                'phpdoc_summary' => true,
-                'return_type_declaration' => true,
-                'single_blank_line_before_namespace' => true,
-                'single_class_element_per_statement' => true,
-                'single_quote' => true,
-                'strict_param' => true,
-                'trailing_comma_in_multiline_array' => true,
-            ] + $extraRules
-        );
+        return CSFC::create($extraRules);
     }
 }
