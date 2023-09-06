7b72ca3271b3a258e285915554a890b1cd31e272
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-09-06 07:36:44 +0200

    Switch composer unused to PHP based configuration format

---
+++ etc/qa/composer-unused.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
+use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
+use Webmozart\Glob\Glob;
+
+return static function (Configuration $config): Configuration {
+    return $config
+        ->addNamedFilter(NamedFilter::fromString('ergebnis/composer-normalize'))
+        ->addNamedFilter(NamedFilter::fromString('icanhazstring/composer-unused'))
+        ->addNamedFilter(NamedFilter::fromString('infection/infection'))
+        ->addNamedFilter(NamedFilter::fromString('maglnet/composer-require-checker'))
+        ->addNamedFilter(NamedFilter::fromString('nunomaduro/collision'))
+        ->addNamedFilter(NamedFilter::fromString('orklah/psalm-insane-comparison'))
+        ->addNamedFilter(NamedFilter::fromString('php-coveralls/php-coveralls'))
+        ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-console-highlighter'))
+        ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-parallel-lint'))
+        ->addNamedFilter(NamedFilter::fromString('php-standard-library/psalm-plugin'))
+        ->addNamedFilter(NamedFilter::fromString('phpstan/phpstan'))
+        ->addNamedFilter(NamedFilter::fromString('psalm/plugin-phpunit'))
+        ->addNamedFilter(NamedFilter::fromString('roave/backward-compatibility-check'))
+        ->addNamedFilter(NamedFilter::fromString('roave/infection-static-analysis-plugin'))
+        ->addNamedFilter(NamedFilter::fromString('squizlabs/php_codesniffer'))
+        ->addNamedFilter(NamedFilter::fromString('vimeo/psalm'))
+        ->addNamedFilter(NamedFilter::fromString('wyrihaximus/coding-standard'))
+        ->addNamedFilter(NamedFilter::fromString('wyrihaximus/phpstan-rules-wrapper'));
+};
