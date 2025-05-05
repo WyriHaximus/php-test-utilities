e01ebd4dbe1db4adbb561a31b34329ca97592c91
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-05-05 13:48:11 +0200

    Add Rector
    This will help code get automagically get upgraded saving a lot of time while doing package upgrades to newer PHP versions. To assist with that a RectoConfig class has been added to this package that will cover 80%+ of all my packages needs.
    
    The running of Rector is already visible in pr this

--- etc/qa/composer-unused.php
+++ etc/qa/composer-unused.php
@@ -4,23 +4,20 @@ declare(strict_types=1);
 
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
-use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
-use Webmozart\Glob\Glob;
 
-return static function (Configuration $config): Configuration {
-    return $config
-        ->addNamedFilter(NamedFilter::fromString('ecoapm/libyear'))
-        ->addNamedFilter(NamedFilter::fromString('ergebnis/composer-normalize'))
-        ->addNamedFilter(NamedFilter::fromString('ergebnis/phpunit-slow-test-detector'))
-        ->addNamedFilter(NamedFilter::fromString('icanhazstring/composer-unused'))
-        ->addNamedFilter(NamedFilter::fromString('infection/infection'))
-        ->addNamedFilter(NamedFilter::fromString('maglnet/composer-require-checker'))
-        ->addNamedFilter(NamedFilter::fromString('php-coveralls/php-coveralls'))
-        ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-console-highlighter'))
-        ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-parallel-lint'))
-        ->addNamedFilter(NamedFilter::fromString('phpstan/phpstan'))
-        ->addNamedFilter(NamedFilter::fromString('roave/backward-compatibility-check'))
-        ->addNamedFilter(NamedFilter::fromString('squizlabs/php_codesniffer'))
-        ->addNamedFilter(NamedFilter::fromString('wyrihaximus/coding-standard'))
-        ->addNamedFilter(NamedFilter::fromString('wyrihaximus/phpstan-rules-wrapper'));
-};
+return static fn (Configuration $config): Configuration => $config
+    ->addNamedFilter(NamedFilter::fromString('ecoapm/libyear'))
+    ->addNamedFilter(NamedFilter::fromString('ergebnis/composer-normalize'))
+    ->addNamedFilter(NamedFilter::fromString('ergebnis/phpunit-slow-test-detector'))
+    ->addNamedFilter(NamedFilter::fromString('icanhazstring/composer-unused'))
+    ->addNamedFilter(NamedFilter::fromString('infection/infection'))
+    ->addNamedFilter(NamedFilter::fromString('maglnet/composer-require-checker'))
+    ->addNamedFilter(NamedFilter::fromString('php-coveralls/php-coveralls'))
+    ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-console-highlighter'))
+    ->addNamedFilter(NamedFilter::fromString('php-parallel-lint/php-parallel-lint'))
+    ->addNamedFilter(NamedFilter::fromString('phpstan/phpstan'))
+    ->addNamedFilter(NamedFilter::fromString('rector/rector'))
+    ->addNamedFilter(NamedFilter::fromString('roave/backward-compatibility-check'))
+    ->addNamedFilter(NamedFilter::fromString('squizlabs/php_codesniffer'))
+    ->addNamedFilter(NamedFilter::fromString('wyrihaximus/coding-standard'))
+    ->addNamedFilter(NamedFilter::fromString('wyrihaximus/phpstan-rules-wrapper'));
